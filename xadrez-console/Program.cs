71c2a2e423025f55051197baf3f6eaae095991c8
Kaue Alves <kaueluizalves@gmail.com>
2025-01-15 12:43:57 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using tabuleiro;
 using xadrez;
 namespace xadrez_console
@@ -9,14 +10,34 @@ namespace xadrez_console
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+               
+<<<<<<< HEAD
+<<<<<<< HEAD
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,5));
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                Tela.imprimirTabuleiro(tab);
+                    partida.executaMovimento(origem, destino);
+                }
+
+
+             
+=======
+
+                Tela.imprimirTabuleiro(partida.tab);
+>>>>>>> 4ede6bc9154137a02efad80f90400f8a86e276b7
+=======
+
+                Tela.imprimirTabuleiro(partida.tab);
+>>>>>>> 4ede6bc9154137a02efad80f90400f8a86e276b7
             }
             catch (TabuleiroException e)
             {
---
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PartidaDeXadrez
+    {
+        public Tabuleiro tab { get; private set;}
+        private int turno;
+        private Cor jogadorAtual;
+<<<<<<< HEAD
+<<<<<<< HEAD
+        public bool terminada { get; private set;}
+=======
+>>>>>>> 4ede6bc9154137a02efad80f90400f8a86e276b7
+=======
+>>>>>>> 4ede6bc9154137a02efad80f90400f8a86e276b7
+
+        public PartidaDeXadrez()
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+<<<<<<< HEAD
+<<<<<<< HEAD
+            terminada = false;
+=======
+>>>>>>> 4ede6bc9154137a02efad80f90400f8a86e276b7
+=======
+>>>>>>> 4ede6bc9154137a02efad80f90400f8a86e276b7
+            colocarPecas();
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.retirarPeca(origem);
+            p.incrementarQteMovimentos();
+            Peca pecaCapturada = tab.retirarPeca(destino);
+            tab.colocarPeca(p, destino);
+
+
+
+        }
+
+        private void colocarPecas()
+        {
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c',1).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c', 2).toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('d', 2).toPosicao());
+
+        }
+
+
+    }
+}
